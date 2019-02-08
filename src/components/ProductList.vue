a61ba6897c91fe4392f9bc8df7fde734d5af58d3
Muhammad Iqmal <root@EGGMTNB010.elken.com>
2019-02-08 18:33:00 +0800

    Learning

---
+++ src/components/ProductList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products"> {{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import shop from '@/api/shop'
+  import store from '@/store/index'
+
+    export default {
+      computed: {
+          products ()
+          {
+              return store.state.products
+          }
+      },
+      created () {
+          // this getProducts here refer to method inside ./api/shop file
+        shop.getProducts(products => {
+          //assign the variable from data() took from ./api/shop file
+          // this.products = products
+
+          //commit the mutation
+          store.commit('setProducts', products)
+        })
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
